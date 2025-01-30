89fcfba5ebe09e76074f4460173027a1edfed93f
Hilton Fernandes <hilton.g.fernandes@gmail.com>
2025-01-30 14:21:47 -0300

    Started the escription of the fields of the test database, and test generation steps

--- doc/test_params.sql
+++ doc/test_params.sql
@@ -1,53 +1,58 @@
 BEGIN TRANSACTION;
-DROP TABLE datafiles;
+
 CREATE TABLE IF NOT EXISTS "datafiles" (
-	"datafile_id"	INTEGER NOT NULL UNIQUE,
-	"timestamp"	    TEXT NOT NULL DEFAULT 'date()',
-	"filename"		TEXT NOT NULL,
+	"datafile_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
+	"filename"	TEXT NOT NULL,
 	"total_recs"	INTEGER NOT NULL,
-	"description"	TEXT NOT NULL,
-	PRIMARY KEY("datafile_id" AUTOINCREMENT)
+	"description"	TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "params" (
-	"param_id"	INTEGER NOT NULL UNIQUE,
-	"algorithm"	TEXT NOT NULL,
+	"param_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"model"		TEXT NOT NULL,
 	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
-	"value"		TEXT NOT NULL,
-	PRIMARY KEY("param_id" AUTOINCREMENT)
+	"value"		TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "code" (
-	"code_id"	INTEGER NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS "paramlims" (
+	"paramlim_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"model"		TEXT NOT NULL,
+	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
+	"extreme_min"	TEXT NOT NULL,
+	"extreme_max"	TEXT NOT NULL,
+	"created_min"	TEXT NOT NULL,
+	"created_max" 	TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "codes" (
+	"code_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
 	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
 	"language"	TEXT NOT NULL,
 	"datafile_id"	INTEGER NOT NULL,
 	"param_id"	INTEGER NOT NULL,
 	FOREIGN KEY (datafile_id) REFERENCES datafiles(datafile_id),	
-	FOREIGN KEY (param_id) REFERENCES params(param_id),	
-	PRIMARY KEY("detail_id" AUTOINCREMENT)
+	FOREIGN KEY (param_id) REFERENCES params(param_id)
 );
 
 CREATE TABLE IF NOT EXISTS "execs" (
-	"exec_id"	INTEGER NOT NULL UNIQUE,
+	"exec_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
 	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
 	"code_id"	INTEGER NOT NULL,
 	"sys_desc"	TEXT NOT NULL,
 	"duration"	NUMERIC NOT NULL,
 	"value"	    TEXT NOT NULL,
-	FOREIGN KEY (code_id) REFERENCES codes(code_id),	
-	PRIMARY KEY("exec_id" AUTOINCREMENT)
+	FOREIGN KEY (code_id) REFERENCES codes(code_id)
 );
 
 CREATE TABLE IF NOT EXISTS "tests" (
-	"test_id"	INTEGER NOT NULL UNIQUE,
+	"test_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
 	"timestamp"	TEXT NOT NULL DEFAULT 'date()',
 	"left_exec_id"	INTEGER NOT NULL,
 	"right_exec_id"	INTEGER NOT NULL,
 	"value"	    TEXT NOT NULL,
 	FOREIGN KEY (left_exec_id) REFERENCES execs(exec_id),	
-	FOREIGN KEY (right_exec_id) REFERENCES execs(exec_id),	
-	PRIMARY KEY("detail_id" AUTOINCREMENT)
+	FOREIGN KEY (right_exec_id) REFERENCES execs(exec_id)
 );
 
 COMMIT;
