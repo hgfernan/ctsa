38dc4e9c2b8b35581c3779d8ff59e5d43f9d50c5
Hilton Fernandes <hilton.g.fernandes@gmail.com>
2025-02-10 22:56:01 -0300

    Started folder `run_test.py`

---
+++ test_gen/run_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Run an automatically generated test
+Created on Mon Feb 10 20:06:16 2025
+
+@author: hilton
+"""
+
+# TODO parse command line 
+# TODO get the executable name and the number of test
+# TODO run the code 
+    # TODO if executable succeeds, 
+        # TODO 
+    # TODO if executable fails, 
+        
