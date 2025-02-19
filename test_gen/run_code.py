bf6cb6d4d722a95a9b67ec303270aad1047e7bac
Hilton Fernandes <hilton.g.fernandes@gmail.com>
2025-02-19 18:18:35 -0300

    Working on `run_code.py`
    The description of all tables should be updated

--- test_gen/run_code.py
+++ test_gen/run_code.py
@@ -8,26 +8,41 @@ Created on Mon Feb 10 20:06:16 2025
 """
 
 import sys      # argv, exit()
+import signal   # Signal
 import os.path  # exists()
 import argparse # class ArgumentParser, class Namespace
+import subprocess # class CalledProcessError, run()
+
 
 from types  import SimpleNamespace
 from typing import List
 
-# TODO parse command line 
-# TODO get the executable name and the number of test
-# TODO run the code 
-    # TODO if executable succeeds, 
-        # TODO save the success report with the name and number of test
-        # TODO updates the test summary with the result
-    # TODO if executable fails, 
-        # TODO save the fail report with the name and number of test
-        # TODO updates the test summary with the result
-  
+def get_executable_root() -> str:
+    return '../Bin/DEBUG'
+
+def bld_range(first : int, last : int = None) -> range:
+    if first is None:
+        msg : str = 'The first parameter must be an integer, not `None`'
+        raise ValueError(msg)
+    
+    _last : int = last
+    if _last is None: 
+        _last = first
+        
+    _first = min(first, _last)
+    _last = max(first, _last)
+    
+    result : range = range(_first, _last + 1)
+
+    # Normal function termination
+    return result        
+    
+    
 def parse_cli(argv : List[str]) -> argparse.Namespace:
     result : argparse.Namespace = argparse.Namespace()
     
-    parser = argparse.ArgumentParser(description='Start a program')
+    desc : str = 'Start sequentially a series of programs'
+    parser = argparse.ArgumentParser(description=desc)
     
     help_str : str = 'Statistical model to be used'
     choices : List[str] = ['AR', 'ARIMA', 'SARIMA', 'SARIMAX']
@@ -43,13 +58,25 @@ def parse_cli(argv : List[str]) -> argparse.Namespace:
                         help=help_str
                        )
     
-    help_str : str = 'Parameter identification number'
-    parser.add_argument('-p', '--parameter', type=int, required=True, 
+    help_str = 'First of a range of parameter identification numbers'
+    parser.add_argument('-P', '--param_first', type=int, required=True,
                         help=help_str
                        )
     
-    help_str : str = 'Data identification number'
-    parser.add_argument('-d', '--data', type=int, required=True, 
+    help_str = 'Last of a range of parameter identification numbers, '
+    help_str += 'including'
+    parser.add_argument('-p', '--param_last', type=int, 
+                        help=help_str
+                       )
+    
+    help_str = 'First of a range of data identification numbers'
+    parser.add_argument('-D', '--data_first', type=int, required=True, 
+                        help=help_str
+                       )
+    
+    help_str = 'First of a range of data identification numbers, '
+    help_str += 'including'
+    parser.add_argument('-d', '--data_last', type=int,
                         help=help_str
                        )
     
@@ -57,31 +84,42 @@ def parse_cli(argv : List[str]) -> argparse.Namespace:
     
     # Normal function termination
     return result 
-  
+
+
+def bld_code_name(model : str, library : str, 
+                  param_id : int, data_id : int) -> str:
+    
+    result : str = model.lower() + '_' + library.lower() + '_'
+    result += f'p{param_id:04d}_d{data_id:04d}'
+    
+    # normal function termination
+    return result
+
+
+def bld_executable_path(library : str, code_name : str) -> str:
+    result : str = library + '/' + code_name
+    
+    # Normal function termination
+    return result 
+
     
 def interpret_args(args : argparse.Namespace) -> SimpleNamespace:
     result : SimpleNamespace = SimpleNamespace()
     
-    code_name : str = ''
+    result.model = args.model 
+    result.library = args.library
     
-    code_name = args.model.lower() + '_' + args.library.lower() + '_'
-    code_name += f'p{args.parameter:04d}_d{args.data:04d}'
+    result.param_range = bld_range(args.param_first, args.param_last)
+    result.param_first, result.param_last = \
+        result.param_range.start, result.param_range.stop - 1
     
-    result.code_name = code_name
-    
-    executable : str = args.library.lower() + '/' + result.code_name
-    if not os.path.exists(executable):
-        msg : str = f'{sys.argv[0]}: ERROR could not find executable '
-        msg += f'\'{executable}\''
-        print(msg)
-        
-        # Return to indicate failure
-        return None
-        
-    result.executable = executable
+    result.data_range = bld_range(args.data_first, args.data_last)
+    result.data_first, result.data_last = \
+        result.data_range.start, result.data_range.stop - 1
     
     # Normal function termination
-    return result 
+    return result
+
 
 def main(argv : List[str]) -> int:
     
@@ -103,6 +141,48 @@ def main(argv : List[str]) -> int:
         return 2 
     
     print(params)
+    
+    for param_id in params.param_range:
+        for data_id in params.data_range:
+            code_name : str = \
+                bld_code_name(params.model, params.library, param_id, data_id)
+            print(code_name)
+            
+            executable_name = \
+                get_executable_root() + '/' +\
+                bld_executable_path(params.library, code_name)
+            
+            try:
+                # TODO get the exec_id from the database
+                exec_id = 1
+                proc = subprocess.run([executable_name, str(exec_id)], 
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, 
+                                    check=True
+                                   )
+                    
+                print(proc.stderr)
+                print(proc.stdout.decode(encoding='utf-8'))
+                
+            except FileNotFoundError as exc:
+                print(f'{type(exc).__name__}: {str(exc)}')
+                print(dir(exc))
+            
+                # print(f'exc.returncode {exc.returncode}')    
+                # print(exc.output)    
+                print(exc.__dict__)
+                
+            except subprocess.CalledProcessError as exc:
+                print(f'{type(exc).__name__}: {str(exc)}')
+                print(dir(exc))
+            
+                print(f'exc.returncode {exc.returncode}')    
+                print(exc.output)    
+                print(exc.__dict__)
+                
+                print(exc.stdout)
+                print(signal.Signals(-exc.returncode).name)
+    
         
     return 0
 
